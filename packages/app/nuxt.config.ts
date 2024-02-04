18a1ee089169c82ded5bf49d0b999692bd1f7622
“bastienr17” <bastienroc@free.fr>
2024-02-04 17:52:37 +0100

    update nuxt.config

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['@unocss/reset/normalize.css', '~/assets/css/main.css'],
   modules: ['@unocss/nuxt', '@nuxt/image', '@nuxtjs/strapi', '@pinia/nuxt'],
   strapi: {
     url: process.env.STRAPI_URL || 'http://localhost:1337',
